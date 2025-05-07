cfd9f15779c1b088bd21be126bed2cde8c0161ac
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-07 17:23:49 +0300

    add server os version

--- server-stats.sh
+++ server-stats.sh
@@ -4,13 +4,15 @@ set -e
 
 # OS version
 
+echo "Server OS: $(grep -o 'PRETTY_NAME=".*"' /etc/os-release | cut -d'"' -f2)"
+
 # Uptime
 
 uptime=$(cat /proc/uptime | awk '{ print int($1) }')
 uptime_hours=$((uptime / 3600))
 uptime_minutes=$(((uptime % 3600) / 60))
 uptime_seconds=$((uptime % 60))
-echo "Uptime: ${uptime_hours}h ${uptime_minutes}m ${uptime_seconds}s"
+echo "   Uptime: ${uptime_hours}h ${uptime_minutes}m ${uptime_seconds}s"
 
 # LA
 
