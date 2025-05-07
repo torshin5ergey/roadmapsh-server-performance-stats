d375b9f6bcccfc24dbb9eb9e421d59824799082c
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-07 17:48:55 +0300

    add cpu total usage, update load average

--- server-stats.sh
+++ server-stats.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
 set -e
+
 
 # OS version
 
-echo "   Server OS: $(grep -o 'PRETTY_NAME=".*"' /etc/os-release | cut -d'"' -f2)"
+echo "      Server OS: $(grep -o 'PRETTY_NAME=".*"' /etc/os-release | cut -d'"' -f2)"
 
 # Uptime
 
@@ -12,15 +12,17 @@ uptime=$(cat /proc/uptime | awk '{ print int($1) }')
 uptime_hours=$((uptime / 3600))
 uptime_minutes=$(((uptime % 3600) / 60))
 uptime_seconds=$((uptime % 60))
-echo "      Uptime: ${uptime_hours}h ${uptime_minutes}m ${uptime_seconds}s"
+echo "         Uptime: ${uptime_hours}h ${uptime_minutes}m ${uptime_seconds}s"
 
 # LA
 
-echo "Load average: $(cat /proc/loadavg | awk '{ print $1, $2, $3}')"
+echo "   Load average: $(cat /proc/loadavg | awk '{ printf "%.1f %.1f %.1f\n", $1, $2, $3 }')"
 
 # Count logged in users
 
 # CPU total usage
+
+cat /proc/stat | grep cpu | head -1 | awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'| awk '{printf "CPU total usage: %.0f%%\n", 100-$1}'
 
 # Mem
 
