3bf319a85db6d8d51ce15d2e84cd21eca3e1cbe0
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-09 13:17:16 +0300

    add logged in users

--- server-stats.sh
+++ server-stats.sh
@@ -3,11 +3,9 @@ set -e
 
 
 # OS version
-
 echo "      Server OS: $(grep -o 'PRETTY_NAME=".*"' /etc/os-release | cut -d'"' -f2)"
 
 # Uptime
-
 uptime=$(cat /proc/uptime | awk '{ print int($1) }')
 uptime_hours=$((uptime / 3600))
 uptime_minutes=$(((uptime % 3600) / 60))
@@ -15,13 +13,13 @@ uptime_seconds=$((uptime % 60))
 echo "         Uptime: ${uptime_hours}h ${uptime_minutes}m ${uptime_seconds}s"
 
 # LA
-
 echo "   Load average: $(cat /proc/loadavg | awk '{ printf "%.1f %.1f %.1f\n", $1, $2, $3 }')"
 
 # Count logged in users
+# /var/run/utmp
+echo "Logged in users: $(who | wc -l)"
 
 # CPU total usage
-
 cat /proc/stat | grep cpu | head -1 | awk '{print ($5*100)/($2+$3+$4+$5+$6+$7+$8+$9+$10)}'| awk '{printf "CPU total usage: %.0f%%\n", 100-$1}'
 
 # Mem
