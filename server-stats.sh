aefe38d265d695ab74c53e4feeea4643ebee89f7
torshin5ergey <ttorshin5ergey@gmail.com>
2025-05-12 15:56:38 +0300

    format outputs

--- server-stats.sh
+++ server-stats.sh
@@ -34,10 +34,10 @@ mem_free_percentage=$((mem_free * 100 / mem_total))
 mem_used=$(free | awk '/Mem/ {print $3}')
 mem_used_percentage=$((mem_used * 100 / mem_total))
 
-echo "   Memory usage:"
-echo "      Total: $((mem_total/1024)) MB"
-echo "       Free: $((mem_free/1024)) MB (${mem_free_percentage}%)"
-echo "       Used: $((mem_used/1024)) MB (${mem_used_percentage}%)"
+echo "=== Memory usage ==="
+echo "  Total: $((mem_total/1024)) MB"
+echo "   Free: $((mem_free/1024)) MB (${mem_free_percentage}%)"
+echo "   Used: $((mem_used/1024)) MB (${mem_used_percentage}%)"
 
 # Disk
 # Total disk usage (Free vs Used including percentage)
@@ -47,10 +47,10 @@ disk_free=$(df -B1 --output=avail / | awk 'NR==2 {print $1}')
 disk_free_percentage=$((disk_free * 100 / disk_total))
 disk_used=$(df -B1 --output=used / | awk 'NR==2 {print $1}')
 disk_used_percentage=$((disk_used * 100 / disk_total))
-echo "     Disk usage:"
-echo "      Total: $((disk_total/1073741824)) GB"
-echo "       Free: $((disk_free/1073741824)) GB (${disk_free_percentage}%)"
-echo "       Used: $((disk_used/1073741824)) GB (${disk_used_percentage}%)"
+echo "=== Disk usage ==="
+echo "  Total: $((disk_total/1073741824)) GB"
+echo "   Free: $((disk_free/1073741824)) GB (${disk_free_percentage}%)"
+echo "   Used: $((disk_used/1073741824)) GB (${disk_used_percentage}%)"
 
 # Top 5 CPU processes
 # ps
@@ -61,5 +61,3 @@ ps -eo pid,user,%cpu,%mem,comm --sort=-%cpu | head -n 6
 # ps
 echo "=== Top 5 Memory processes ==="
 ps -eo pid,user,%mem,%cpu,comm --sort=-%mem | head -n 6
-
-# Count failed login attempts
